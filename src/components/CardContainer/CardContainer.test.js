f8e1f1a01c83f1e7a127b1115bb30d2a08e7d7e1
Spencer Herms <slherms@gmail.com>
2018-02-22 15:33:29 -0700

    add snapshot test

---
+++ src/components/CardContainer/CardContainer.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { CardContainer } from './CardContainer';
+
+describe('CardContainer', () => {
+  let wrapper
+
+  const mockData = [
+    {
+      house: 'stark'
+    },
+    {
+      house: 'lannister'
+    }
+  ];
+  
+  beforeEach( () => {
+    wrapper= shallow( <CardContainer houseData={mockData} /> )
+  })
+
+  it( 'exist and matches snapshot', () => {
+    expect(wrapper).toBeDefined();
+    expect(wrapper).toMatchSnapshot();
+  })
+
+})
