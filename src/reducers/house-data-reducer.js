c318f91d8d5cea215c17758aef78fdd7dfb77e92
Spencer Herms <slherms@gmail.com>
2018-02-22 13:30:20 -0700

    formats to remove linter errors

--- src/reducers/house-data-reducer.js
+++ src/reducers/house-data-reducer.js
@@ -1,7 +1,7 @@
-const houseDataReducer = (state=[], action) => {
+const houseDataReducer = (state = [], action) => {
   switch (action.type) {
   case 'SET_HOUSE_DATA':
-    return action.houseData
+    return action.houseData;
   default:
     return state;
   }
