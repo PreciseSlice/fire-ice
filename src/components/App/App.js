2ec59bc3be467eac87cf9a8426449aa8b523d3b3
Spencer Herms <slherms@gmail.com>
2018-02-22 13:03:31 -0700

    add gif logic, render card container

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+//import PropTypes from 'prop-types'
 import logo from './logo.svg';
 import './App.css';
 import { connect } from 'react-redux';
 import { setHouseData } from '../../actions/index';
 import { getHouses } from '../helpers/apiCalls';
-
+import wolf from './wolf.gif';
+import CardContainer from '../CardContainer/CardContainer'
 
 export class App extends Component {
   constructor(props) {
@@ -15,38 +16,43 @@ export class App extends Component {
   async componentDidMount() {
     const { setHouseData } = this.props;
   
-    const houseData = await getHouses()
+    const fetchHouseData = await getHouses()
     
-    setHouseData(houseData)
+    setHouseData(fetchHouseData)
   }
 
   render() {
-    return (
-      <div className='App'>
-        <div className='App-header'>
-          <img src={logo} className='App-logo' alt='logo' />
-          <h2>Welcome to Westeros</h2>
-          <button onClick={() => {
-            this.props.fakeAction();
-            alert(this.props.fake);
-          }}> FAKE ACTION</button>
+    const { houseData } = this.props;
+    if (houseData.length < 1) {
+      return (
+        <img src={wolf} className='wolf-gif' alt='wolf' />
+      )
+    } else {
+      return (
+        <div className='App'>
+          <div className='App-header'>
+            <img src={logo} className='App-logo' alt='logo' />
+            <h2>Welcome to Westeros</h2>
+            <button onClick={() => {
+              this.props.fakeAction();
+              alert(this.props.fake);
+            }}> FAKE ACTION</button>
+          </div>
+          <div className='Display-info'>
+            <CardContainer />
+          </div>
         </div>
-        <div className='Display-info'>
-        </div>
-      </div>
-    );
+      );
+    }
   }
 }
 
 App.propTypes = {
-  //fake: PropTypes.shape({ fake: PropTypes.string }),
-  //fakeAction: PropTypes.func.isRequired
+
 };
 
 export const mapStateToProps = state => ({
-   houseData: state.houseData,
-   log: console.log(state.houseData)
-    
+   houseData: state.houseData
 });
 
 
