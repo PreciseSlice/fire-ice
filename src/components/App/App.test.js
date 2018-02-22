50a9ced5547e9a355b91fa5fabefce8593c578dd
Spencer Herms <slherms@gmail.com>
2018-02-22 15:32:20 -0700

    add snapshot test

---
+++ src/components/App/App.test.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { App } from './App';
+
+describe('App', () => {
+  let wrapper;
+  let mockFunction = jest.fn()
+
+  window.fetch = jest.fn().mockImplementation(() => ({
+    status: 200,
+    json: () =>
+      new Promise((resolve, reject) => {
+        resolve(mockData);
+      })
+  }));
+
+  const mockData = [
+    {
+      house: 'stark'
+    },
+    {
+      house: 'lannister'
+    }
+  ];
+
+  beforeEach(() => {
+    wrapper = shallow(<App houseData={mockData} setHouseData={mockFunction} />);
+  });
+
+  it('exist and matches snapshot', () => {
+    expect(wrapper).toBeDefined();
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('maps state to props', () => {
+
+  })
+
+  it('maps dispatch to props', () => {
+
+  })
+  
+});
