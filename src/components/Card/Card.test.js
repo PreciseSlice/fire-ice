9b9942828740bba2a95bf59991d201c18f8f8827
Spencer Herms <slherms@gmail.com>
2018-02-22 15:33:11 -0700

    mocks data for test to run

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Card from './Card';
+import { Card } from './Card';
 
 describe('Card', () => {
   let wrapper
 
+  const mockData = {
+    item: 'string'
+  } 
+  
+
   beforeEach( () => {
-    wrapper= shallow( <Card /> )
+    wrapper= shallow( <Card data={mockData} /> )
   })
 
   it( 'exist and matches snapshot', () => {
     expect(wrapper).toBeDefined();
     expect(wrapper).toMatchSnapshot();
-  } )
+  })
 })
