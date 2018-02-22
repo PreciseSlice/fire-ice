a5baf5f6183e04a31d5acb17f2645582d7e7f76f
Spencer Herms <slherms@gmail.com>
2018-02-22 13:29:45 -0700

    formats to remove linter errors

--- src/components/helpers/apiCalls.js
+++ src/components/helpers/apiCalls.js
@@ -1,21 +1,21 @@
 export const callFetch = async url => {
   try {
     const getData = await fetch(url);
-  
-    if(getData.status <= 200) {
-      return await getData.json()
+
+    if (getData.status <= 200) {
+      return await getData.json();
     } else {
-      throw new Error ('status code > 200')
+      throw new Error('status code > 200');
     }
   } catch (error) {
-    throw new Error (error)
+    throw new Error(error);
   }
-}
+};
 
 export const getHouses = async () => {
   const fetchHouseData = await callFetch('http://localhost:3001/api/v1/houses');
-  
-  const houseMap = await fetchHouseData.map( async house => {
+
+  const houseMap = await fetchHouseData.map(async house => {
     return {
       name: house.name,
       coatOfArms: house.coatOfArms,
@@ -24,8 +24,8 @@ export const getHouses = async () => {
       seats: house.seats,
       founded: house.founded,
       ancestralWeapons: house.ancestralWeapons
-    }
-  })
-  
+    };
+  });
+
   return Promise.all(houseMap);
-}
+};
