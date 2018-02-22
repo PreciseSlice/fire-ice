984efd2098546d2e9ae828fb8283cb0ed1c17249
Spencer Herms <slherms@gmail.com>
2018-02-22 15:59:34 -0700

    add testing

--- src/components/helpers/api.test.js
+++ src/components/helpers/api.test.js
@@ -1,8 +1,18 @@
+/* eslint-disable */
 import React from 'react';
 import { callFetch, getHouses } from './apiCalls';
 
 describe('Api call test', () => {
-  let mockFunction = jest.fn()
+  let mockFunction = jest.fn();
+
+  const mockData = [
+    {
+      house: 'stark'
+    },
+    {
+      house: 'lannister'
+    }
+  ];
 
   window.fetch = jest.fn().mockImplementation(() => ({
     status: 200,
@@ -10,18 +20,36 @@ describe('Api call test', () => {
       new Promise((resolve, reject) => {
         resolve(mockData);
       })
-  }))
+  }));
 
   it('calls fetchs', () => {
+    const expectedParams = 'http://localhost:3001/api/v1/houses'
+    window.fetch = jest.fn().mockImplementation(() => ({
+      status: 200,
+      json: () =>
+        new Promise((resolve, reject) => {
+          resolve(mockData);
+        })
+    }));
 
-  })
+    callFetch(expectedParams)
+    expect(window.fetch).toHaveBeenCalledWith(expectedParams)
+  });
 
   it('returns the data object if the status is above 200', () => {
+    const url = 'http://localhost:3001/api/v1/houses'
 
-  })
+    expect(callFetch(url)).resolves.toEqual(mockData)
+  });
 
   it('throws and error if the data is not fetched', () => {
+    window.fetch = jest.fn().mockImplementation(() => ({
+      status: 500,
+      json: () =>
+        new Promise((resolve, reject) => {
+          reject();
+        })
+    }));
     
-  })
-
-})
+  });
+});
