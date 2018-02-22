f2386411fe93b262c03845a581bb312d5b323e14
Spencer Herms <slherms@gmail.com>
2018-02-22 15:59:19 -0700

    formats

--- src/components/Card/Card.test.js
+++ src/components/Card/Card.test.js
@@ -3,19 +3,18 @@ import { shallow } from 'enzyme';
 import { Card } from './Card';
 
 describe('Card', () => {
-  let wrapper
+  let wrapper;
 
   const mockData = {
     item: 'string'
-  } 
-  
+  };
 
-  beforeEach( () => {
-    wrapper= shallow( <Card data={mockData} /> )
-  })
+  beforeEach(() => {
+    wrapper = shallow(<Card data={mockData} />);
+  });
 
-  it( 'exist and matches snapshot', () => {
+  it('exist and matches snapshot', () => {
     expect(wrapper).toBeDefined();
     expect(wrapper).toMatchSnapshot();
-  })
-})
+  });
+});
