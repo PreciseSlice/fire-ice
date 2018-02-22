184c9444ddfd2d14316e2d59cbb64419ff77cb13
Spencer Herms <slherms@gmail.com>
2018-02-22 14:03:53 -0700

    first action test

---
+++ src/actions/action.test.js
@@ -0,0 +1,23 @@
+import * as actions from './index';
+import { createStore } from 'redux';
+import rootReducer from '../reducers/root-reducer';
+
+describe('actions', () => {
+  const houseData = [
+    {
+      house: 'stark'
+    },
+    {
+      house: 'lannister'
+    }
+  ]
+
+  it('should reurn a type of SET_HOUSE_DATA with the housData payload', () => {
+    const expected = {
+      type: 'SET_HOUSE_DATA',
+      houseData
+    }
+    expect(actions.setHouseData(houseData)).toEqual(expected)
+  })
+
+})
